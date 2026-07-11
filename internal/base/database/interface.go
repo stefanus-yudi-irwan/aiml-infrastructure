c302e7027b3127cc92253737a623b301c6302f01
Stefanus Yudi Irwan <stefanus.yudi96@gmail.com>
2026-07-11 21:06:49 +0700

    feat: add postgres-exporter

--- internal/base/database/interface.go
+++ internal/base/database/interface.go
@@ -1,14 +1,17 @@
 package database
 
+type DataStruct interface{}
+type IsDataExists bool
+
 type IDBConnector interface {
-	Insert(structPointer interface{}) error
-	Update(structPointer interface{}, structFields ...string) error
-	Upsert(structPointer interface{}) error
-	HardDelete(structPointer interface{}) error
-	SoftDelete(structPointer interface{}) error
-	Restore(structPointer interface{}) error
-	GetByPrimaryKeys(structPointer interface{}) error
-	Exists(structPointer interface{}) (bool, error)
+	Insert(structPointer DataStruct) error
+	Update(structPointer DataStruct, structFields ...string) error
+	Upsert(structPointer DataStruct) error
+	HardDelete(structPointer DataStruct) error
+	SoftDelete(structPointer DataStruct) error
+	Restore(structPointer DataStruct) error
+	GetByPrimaryKeys(structPointer DataStruct) error
+	Exists(structPointer DataStruct) (IsDataExists, error)
 	Ping(timeLimitSecond int) error
 	Close() error
 }
